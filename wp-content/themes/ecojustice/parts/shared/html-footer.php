a23b8c8b0b3f5c6b378058f2063e9d54bd393bea
jeremybutler <jeremyabutler@gmail.com>
2014-03-12 17:16:36 -0700

    EcoJustice theme and Starkers
    The FTP directory seemed to be diffrent then git, so just trying to make
    them match them out.

---
+++ wp-content/themes/ecojustice/parts/shared/html-footer.php
@@ -0,0 +1,7 @@
+	<?php wp_footer(); ?>
+	
+	<!-- fontoids -->
+	<script type="text/javascript" src="http://fast.fonts.net/jsapi/8af6e621-0909-49d2-9325-4a3bc22f98c6.js"></script>
+	
+	</body>
+</html>
